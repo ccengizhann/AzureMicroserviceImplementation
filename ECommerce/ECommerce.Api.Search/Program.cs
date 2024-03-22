e7e2b75f8b3def804b8ed2354176b0c5b1f3d203
Cengizhan <civelekc18@itu.edu.tr>
2024-03-22 21:24:37 +0300

    Added searchService.api project.

--- ECommerce/ECommerce.Api.Search/Program.cs
+++ ECommerce/ECommerce.Api.Search/Program.cs
@@ -1,5 +1,29 @@
+using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Services;
+using Polly;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//Dependency Injection
+builder.Services.AddScoped<ISearchService, SearchService>();
+builder.Services.AddScoped<IOrdersService , OrdersService>();
+builder.Services.AddScoped<IProductsService , ProductsService>();
+builder.Services.AddScoped<ICustomersSevice , CustomersService>();
+
+builder.Services.AddHttpClient("OrdersService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Orders"]);
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+
+builder.Services.AddHttpClient("CustomersService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Customers"]);
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
+
+builder.Services.AddHttpClient("ProductsService", config =>
+{
+    config.BaseAddress = new Uri(builder.Configuration["Services:Products"]);
+}).AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(5, _ => TimeSpan.FromMilliseconds(500)));
 // Add services to the container.
 
 builder.Services.AddControllers();
